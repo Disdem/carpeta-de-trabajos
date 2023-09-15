34edbf060105b39a9ea063faad37dce05850a856
Disdem <zap1051@amerike.edu.mx>
2023-09-15 10:56:19 -0600

    disdem
    Cambios por Asesoria con Mau

---
+++ Luis datos pls no borrar/Asesoria Mau/Asesoria Mau.cpp
@@ -0,0 +1,28 @@
+// Asesoria Mau.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    int edad = 0;
+    bool HoM = true;
+    char inicial_nombre;
+    char nombre[10];
+    std::cout << "Hola, introduce tu edad;" <<
+        std::endl;
+    std::cin >> edad;
+    std::cout << "asi que tienes " <<
+        edad << " años" << std::endl;
+    
+    std::cout << "Usuario dime eres Hombre o Mujer;" <<
+        std::endl;
+    std::cin >> HoM;
+    std::cout << "Asi que eres " <<
+        HoM << "Genero" << std::endl;
+
+
+
+    
+
+}
